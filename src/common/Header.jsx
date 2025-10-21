dcd302596f72f5fdc3961c70acd7fc3becf9053a
Automation maintenance <dakuyomath@gmail.com>
2025-10-21 17:22:44 +0100

    fourth commit

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,85 +1,77 @@
 import React, { Component, createRef } from "react";
 import { Link } from "react-scroll";
 
-export class Header extends Component {  
-    constructor(props) {
-        super(props);
-        this.headerRef = createRef();
-        this.state = {
-            headerHeight: 80
-        };
-    }
+export class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.headerRef = createRef();
+    this.state = {
+      headerHeight: 80,
+      menuActive: false, // 👈 état du menu
+    };
+  }
 
-    componentDidMount() {
-        if (this.headerRef.current) {
-            const height = this.headerRef.current.offsetHeight;
-            this.setState({ headerHeight: height });
-        }
+  componentDidMount() {
+    if (this.headerRef.current) {
+      const height = this.headerRef.current.offsetHeight;
+      this.setState({ headerHeight: height });
     }
+  }
 
-    render(){
-        const { headerHeight } = this.state;
-        const { onIconClick } = this.props;
-        
-        return(
-            <header className="header" ref={this.headerRef}>
-                
-                {/* LOGO */}
-                <Link 
-                    to="home" 
-                    smooth={true} 
-                    duration={500} 
-                    offset={-headerHeight} 
-                    className="logo"
-                > 
-                    <i className="fas fa-utensils"></i> L'AVENIR 
-                </Link>
-                
-                {/* NAVIGATION */}
-                <nav className="navbar">
-                    <Link to="home" smooth={true} duration={500} offset={-headerHeight}>acceuil</Link>
-                    <Link to="about" smooth={true} duration={500} offset={-headerHeight}>à propos</Link>
-                    <Link to="popular" smooth={true} duration={500} offset={-headerHeight}>populaire</Link>
-                    <Link to="menu" smooth={true} duration={500} offset={-headerHeight}>menu</Link>
-                    <Link to="order" smooth={true} duration={500} offset={-headerHeight}>commander</Link>
-                    <Link to="blogs" smooth={true} duration={500} offset={-headerHeight}>blogs</Link>
-                </nav>
+  toggleMenu = () => {
+    this.setState(prev => ({ menuActive: !prev.menuActive }));
+  };
 
-                {/* ICÔNES */}
-                <div className="icons">
-                    
-                    {/* Bouton menu */}
-                    <Link to="menu" smooth={true} duration={500} offset={-headerHeight}>
-                        <div id="menu-btn" className="fas fa-bars"></div>
-                    </Link>
-                    
-                    {/* Groupe des modales */}
-                    <div className="modals-container">
-                        <div 
-                            onClick={() => onIconClick('search')} 
-                            className="modal-icon"
-                        >
-                            <i className="fas fa-search"></i>
-                        </div>
-                        
-                        <div 
-                            onClick={() => onIconClick('shopping')} 
-                            className="modal-icon"
-                        >
-                            <i className="fas fa-shopping-cart"></i>
-                        </div>
-                        
-                        <div 
-                            onClick={() => onIconClick('login')} 
-                            className="modal-icon"
-                        >
-                            <i className="fas fa-user"></i>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        )
-    }
+  render() {
+    const { headerHeight, menuActive } = this.state;
+    const { onIconClick } = this.props;
+
+    return (
+      <header className="header" ref={this.headerRef}>
+        {/* LOGO */}
+        <Link
+          to="home"
+          smooth={true}
+          duration={500}
+          offset={-headerHeight}
+          className="logo"
+        >
+          <i className="fas fa-utensils"></i> L'AVENIR
+        </Link>
+
+        {/* NAVIGATION */}
+        <nav className={`navbar ${menuActive ? "active" : ""}`}>
+          <Link to="home" smooth={true} duration={500} offset={-headerHeight}>acceuil</Link>
+          <Link to="about" smooth={true} duration={500} offset={-headerHeight}>à propos</Link>
+          <Link to="popular" smooth={true} duration={500} offset={-headerHeight}>populaire</Link>
+          <Link to="menu" smooth={true} duration={500} offset={-headerHeight}>menu</Link>
+          <Link to="order" smooth={true} duration={500} offset={-headerHeight}>commander</Link>
+          <Link to="blogs" smooth={true} duration={500} offset={-headerHeight}>blogs</Link>
+        </nav>
+
+        {/* ICÔNES */}
+        <div className="icons">
+          {/* Bouton menu */}
+          <div id="menu-btn" className="fas fa-bars" onClick={this.toggleMenu}></div>
+
+          {/* Groupe des modales */}
+          <div className="modals-container">
+            <div onClick={() => onIconClick('search')} className="modal-icon">
+              <i className="fas fa-search"></i>
+            </div>
+
+            <div onClick={() => onIconClick('shopping')} className="modal-icon">
+              <i className="fas fa-shopping-cart"></i>
+            </div>
+
+            <div onClick={() => onIconClick('login')} className="modal-icon">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default Header;
